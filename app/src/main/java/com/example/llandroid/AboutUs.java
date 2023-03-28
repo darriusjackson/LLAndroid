4c0233901c95f835847ecf1dd92749cb57062e0a
DJ Jackson <jacksond02@lsus.edu>
2023-03-27 22:19:13 -0500

    UI designs from Android Studio

--- app/src/main/java/com/example/llandroid/AboutUs.java
+++ app/src/main/java/com/example/llandroid/AboutUs.java
@@ -1,6 +1,7 @@
 package com.example.llandroid;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -39,6 +40,8 @@ public class AboutUs extends AppCompatActivity {
 
                 if (fullName.isEmpty() && email.isEmpty() && message.isEmpty()) {
                     Toast.makeText(AboutUs.this, "Enter all data", Toast.LENGTH_LONG).show();
+                }else {
+                    startActivity(new Intent(AboutUs.this, ProfilePage.class));
                 }
 
                 db2.AboutUsAddDBRecord(fullName, email, message);
